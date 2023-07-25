a9922c4a63e9c50dc77494411f66c5f53548a414
nvaughan00 <nickvaughan123@gmail.com>
2023-07-25 15:28:35 -0500

    Updates

--- Budgeting/Backend/hello.js
+++ Budgeting/Backend/hello.js
@@ -3,6 +3,8 @@ const app = express();
 const mysql = require('mysql');
 const cors = require('cors')
 
+const monthRanges = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
 app.use(express.json());
 app.use(cors({
     origin: '*'
@@ -16,9 +18,38 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
-//READ Request Handlers
-app.get('/', (req, res) => {
+//Get all static expenses for nick
+app.get('/nick', (req, res) => {
     connection.query("SELECT * FROM NICK_EXPENSES", function (err, result, fields) {
+        if (err) {
+            throw err;
+        }
+        else {
+            res.send(result);
+        }
+    });
+});
+
+//Get all static expenses for Lilian
+app.get('/lilian', (req, res) => {
+    connection.query("SELECT * FROM LILIAN_EXPENSES", function (err, result, fields) {
+        if (err) {
+            throw err;
+        }
+        else {
+            res.send(result);
+        }
+    });
+});
+
+
+//Get all variable expenses for Nick
+app.get('/nick/:year-:month', (req, res) => {
+    var start = `${req.params.year}-${req.params.month}-01 00:00:00`;
+    var end = `${req.params.year}-${req.params.month}-${monthRanges[req.params.month - 1]} 23:59:59`;
+    var query = `SELECT * FROM NICK_EXPENSES_VARIABLE WHERE TransactionDate BETWEEN '${start}' AND '${end}'`
+
+    connection.query(query, function (err, result, fields) {
         if (err) {
             throw err;
         }
