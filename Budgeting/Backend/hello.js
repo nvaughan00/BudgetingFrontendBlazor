34c08d6d9b6cb6f705476d1a9818370e254667c9
nvaughan00 <46831906+nvaughan00@users.noreply.github.com>
2023-07-26 00:20:49 -0500

    change

--- Budgeting/Backend/hello.js
+++ Budgeting/Backend/hello.js
@@ -2,13 +2,19 @@
 const app = express();
 const mysql = require('mysql');
 const cors = require('cors')
+const bodyParser = require('body-parser');
 
 const monthRanges = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 app.use(express.json());
-app.use(cors({
-    origin: '*'
-}));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(
+    cors({
+        origin: '*'
+    }
+    )
+);
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -45,16 +51,35 @@ app.get('/lilian', (req, res) => {
 
 //Get all variable expenses for Nick
 app.get('/nick/:year-:month', (req, res) => {
-    var start = `${req.params.year}-${req.params.month}-01 00:00:00`;
-    var end = `${req.params.year}-${req.params.month}-${monthRanges[req.params.month - 1]} 23:59:59`;
-    var query = `SELECT * FROM NICK_EXPENSES_VARIABLE WHERE TransactionDate BETWEEN '${start}' AND '${end}'`
+    const start = `${req.params.year}-${req.params.month}-01 00:00:00`;
+    const end = `${req.params.year}-${req.params.month}-${monthRanges[req.params.month - 1]} 23:59:59`;
+    const query = `SELECT * FROM NICK_EXPENSES_VARIABLE WHERE TransactionDate BETWEEN '${start}' AND '${end}'`;
 
     connection.query(query, function (err, result, fields) {
         if (err) {
             throw err;
         }
         else {
-            res.send(JSON.stringify(result));
+            res.send(result);
+        }
+    });
+});
+
+app.post('/nick/addexpense', (req, res) => {
+
+    const value = req.body.Amount;
+    const description = req.body.Description;
+    const date = req.body.TransactionDate;
+    const query = `INSERT INTO NICK_EXPENSES_VARIABLE(TransactionDate, Amount, Description) VALUES('${date}', ${value}, '${description}')`;
+
+    console.log(query);
+
+    connection.query(query, function (err) {
+        if (err) {
+            throw err;
+        }
+        else {
+            res.ok;
         }
     });
 });
